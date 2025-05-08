3ad17ab472e8027aec01472ec3c0a75eadcfbc60
¨Cordeiro <cordeiroluisff@gmail.com>
2025-05-08 10:41:47 +0100

    feat: Validação dos formulários, usando o zod

--- aulas/02-ignite-timer/src/pages/Home/Index.tsx
+++ aulas/02-ignite-timer/src/pages/Home/Index.tsx
@@ -1,4 +1,8 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from  "zod";
+
 import {
   CountDownContainer,
   FormContainer,
@@ -9,13 +13,36 @@ import {
   TaskInput
 } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod.number()
+  .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+  .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+})
+
 export function Home() {
+const { register, handleSubmit, watch } = useForm({
+  resolver: zodResolver(newCycleFormValidationSchema),
+});
+
+  function handleCreateNewCicle(data: any) {
+    console.log(data)
+  } 
+
+  const task = watch("task");
+  const isSubmitDesabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCicle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projecto" />
+          <TaskInput 
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projecto"
+            {...register("task")}
+          />
 
           <datalist id="task-suggestions">
              <option value="projeto 1"></option>
@@ -30,10 +57,9 @@ export function Home() {
             placeholder="00"
             step={5} 
             min={5}
-            max={60}
+           /*  max={60} */
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
-
-          
 
           <span>minutos.</span>
         </FormContainer>
@@ -46,7 +72,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={isSubmitDesabled}  type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
